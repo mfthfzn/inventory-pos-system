59827a2bf0d78c959dd323d99daf9c0a7d000a26
mfthfzn <miftahulfauzan55@gmail.com>
2026-02-05 20:59:23 +0800

    feat: redis util

---
+++ java-backend/src/main/java/io/github/mfthfzn/util/RedisUtil.java
@@ -0,0 +1,37 @@
+package io.github.mfthfzn.util;
+
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.StatefulRedisConnection;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.Duration;
+
+@Slf4j
+public class RedisUtil {
+
+  private static RedisClient redisClient;
+
+  @Getter
+  private static final StatefulRedisConnection<String, String> connection;
+
+  static {
+    RedisURI redisURI = RedisURI.builder()
+            .withHost("localhost")
+            .withPort(6379)
+            .withDatabase(0)
+            .withTimeout(Duration.ofSeconds(5))
+            .build();
+
+    redisClient = RedisClient.create(redisURI);
+    connection = redisClient.connect();
+  }
+
+  public static void shutdown() {
+    connection.close();
+    redisClient.close();
+    log.info("Close redis connection");
+  }
+
+}
